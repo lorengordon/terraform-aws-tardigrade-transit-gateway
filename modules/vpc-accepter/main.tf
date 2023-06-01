66710809e02d9f30e5560fda431b09d99b0ac841
Ehinome Ihenyen <ehi.ihenyen@gmail.com>
2023-06-01 12:38:35 -0400

    Adding count argument for transit gateway

--- modules/vpc-accepter/main.tf
+++ modules/vpc-accepter/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "this" {
-  transit_gateway_attachment_id = var.transit_gateway_attachment_id
+  count = var.auto_accept_shared_attachments ? 0 : 1
 
+  transit_gateway_attachment_id                   = var.transit_gateway_attachment_id
   transit_gateway_default_route_table_association = var.transit_gateway_default_route_table_association
   transit_gateway_default_route_table_propagation = var.transit_gateway_default_route_table_propagation
 
